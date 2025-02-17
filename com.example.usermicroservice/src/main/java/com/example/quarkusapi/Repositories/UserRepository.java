d1fa380a600e83c23090f2e02869a3327f91cf9f
Davi0805 <melocardozo.video@gmail.com>
2025-02-17 23:08:45 +0000

    .

--- com.example.usermicroservice/src/main/java/com/example/quarkusapi/Repositories/UserRepository.java
+++ com.example.usermicroservice/src/main/java/com/example/quarkusapi/Repositories/UserRepository.java
@@ -3,10 +3,24 @@ package com.example.quarkusapi.Repositories;
 import com.example.quarkusapi.model.User;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.hibernate.Hibernate;
 
 @ApplicationScoped
 public class UserRepository implements PanacheRepository<User> {
     public User findUserWithCompanies(String username) {
         return find("SELECT u FROM User u LEFT JOIN FETCH u.userCompanies uc LEFT JOIN FETCH uc.company WHERE u.username = ?1", username).firstResult();
     }
+
+//public User findUserWithCompanies(String username) {
+//    // Buscar apenas o usuário (sem JOIN FETCH)
+//    User user = find("SELECT u FROM User u WHERE u.username = ?1", username).firstResult();
+//
+//    // Se encontrou o usuário, carregar suas empresas separadamente
+//    if (user != null) {
+//        Hibernate.initialize(user.getUserCompanies());
+//    }
+//
+//    return user;
+//}
+
 }
