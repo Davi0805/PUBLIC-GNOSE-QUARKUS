59b1ec78a26999499dc269be71c6170e0d99362e
Davi0805 <melocardozo.video@gmail.com>
2025-03-14 00:24:18 +0000

    refac(Models): User constructor added

--- com.example.usermicroservice/src/main/java/com/example/quarkusapi/model/User.java
+++ com.example.usermicroservice/src/main/java/com/example/quarkusapi/model/User.java
@@ -15,6 +15,17 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Table(name = "users")
 public class User implements Serializable
 {
+    public User() {}
+
+    public User(String username, String password, String email, String first_name, String last_name)
+    {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
